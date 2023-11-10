dce5b880afd9c10cbbc8018e06d3dcf7f20dbe6a
Arman Nakhwa <armannakhwa19@gmail.com>
2023-11-10 15:01:19 +0530

    first

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const connectDb = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const quizRoutes = require("./routes/quizRoutes");
+const performanceRoutes = require("./routes/performanceRoutes");
+const cors = require("cors");
+const path = require("path");
+dotenv.config();
+
+connectDb();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+// app.get("/", (req, res) => {
+//   res.status(200).send("App is running successfully!");
+// });
+
+
+app.use("/api/user", userRoutes);
+app.use("/api/quiz", quizRoutes);
+app.use("/api/performance", performanceRoutes);
+
+const port = process.env.PORT || 5000;
+
+app.use(express.static(path.resolve(__dirname, './client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client//build/index.html'));
+});
+
+
+app.listen(port, () => {
+  console.log(
+    `Server is running on http://localhost:${port}`.cyan.bgWhite.bold
+  );
+});
